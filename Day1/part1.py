dc91dd9b9e324f4f9f82c188bdf45cc991126012
rvan-duy <rvanduyneveldt@gmail.com>
2021-12-01 13:57:56 +0100

    part 1

---
+++ Day1/part1.py
@@ -0,0 +1,17 @@
+# Day 1: Sonar Sweep
+# How many measurements are larger than the previous measurement?
+# Author: Ruben van Duyneveldt
+
+file_input = open('input.txt', 'r')
+lines = file_input.readlines()
+file_input.close()
+
+previous = 0
+count = 0
+for line in lines:
+    if int(line) > previous:
+        count = count + 1
+    previous = int(line)
+
+count = count - 1
+print(count)
