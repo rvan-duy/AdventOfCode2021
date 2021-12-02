03ccc21d7425c34b56f420c1ee8f2690e271649e
Ruben van Duyneveldt <rvanduyneveldt@gmail.com>
2021-12-02 13:30:18 +0100

    part2

---
+++ Day2/part2.py
@@ -0,0 +1,25 @@
+# Day 2: Dive!
+# What do you get if you multiply your final horizontal position by your final depth?
+# Author: Ruben van Duyneveldt
+
+f = open('input.txt', 'r')
+instructions = f.readlines()
+f.close()
+
+depth = 0
+horizontal = 0
+aim = 0
+for instruction in instructions:
+
+    list_item = instruction.split(' ')
+    list_item[1] = list_item[1][:-1]
+
+    if list_item[0] == 'forward':
+        horizontal += int(list_item[1])
+        depth += aim * int(list_item[1])
+    elif list_item[0] == 'down':
+        aim += int(list_item[1])
+    elif list_item[0] == 'up':
+        aim -= int(list_item[1])
+
+print('Result: ' + str(horizontal * depth))
