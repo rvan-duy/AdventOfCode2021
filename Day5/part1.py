96cd0889451a5fdde7e02fbc8add75ec22845cc4
rvan-duy <rvanduyneveldt@gmail.com>
2021-12-05 12:47:16 +0100

    part1 done

---
+++ Day5/part1.py
@@ -0,0 +1,49 @@
+file = open('input.txt')
+file_input = file.readlines()
+file.close()
+
+max_x, max_y = 0, 0
+for i in range(len(file_input)):
+    file_input[i] = file_input[i][:-1].split()
+    file_input[i].remove('->')
+    file_input[i][0] = file_input[i][0].split(',')
+    file_input[i][1] = file_input[i][1].split(',')
+    max_x = max(max_x, int(file_input[i][0][0]), int(file_input[i][1][0]))
+    max_y = max(max_y, int(file_input[i][0][1]), int(file_input[i][1][1]))
+
+diagram = []
+x, y = 0, 0
+while x < max_x + 1:
+    diagram.append([])
+    y = 0
+    while y < max_y + 1:
+        diagram[x].append(0)
+        y += 1
+    x += 1
+
+for entry in file_input:
+    if entry[0][0] != entry[1][0] and entry[0][1] != entry[1][1]:
+        continue
+    elif entry[0][0] != entry[1][0]:
+        end = max(int(entry[0][0]), int(entry[1][0]))
+        start = min(int(entry[0][0]), int(entry[1][0]))
+        for i in range(start, end + 1):
+            diagram[int(entry[0][1])][i] += 1
+    elif entry[0][1] != entry[1][1]:
+        end = max(int(entry[0][1]), int(entry[1][1]))
+        start = min(int(entry[0][1]), int(entry[1][1]))
+        for i in range(start, end + 1):
+            diagram[i][int(entry[0][0])] += 1
+
+total_overlap_points = 0
+for row in diagram:
+    for element in row:
+        if element > 1:
+            total_overlap_points += 1
+    print(row)
+
+print('Amount of times points overlap:', total_overlap_points)
+
+# print(max_x, max_y)
+# print(file_input)
+
