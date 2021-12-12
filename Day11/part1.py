a477b4749f698d32d7373ab3724336a12f65082e
Ruben van Duyneveldt <rvanduyneveldt@gmail.com>
2021-12-12 14:28:23 +0100

    day11 part1 done

--- Day11/part1.py
+++ Day11/part1.py
@@ -5,76 +5,83 @@
 # -- if octopus is then > 9 it also flashes
 # - if octopus has flashed, it goes back to 0
 
-input = open('simple_input.txt', 'r')
+input = open('input.txt', 'r')
 octopuses = input.readlines()
 for i in range(len(octopuses)):
     octopuses[i] = octopuses[i][:-1]
     octopuses[i] = list(map(int, octopuses[i]))
 
 
-def reset_flashed(octopuses):
-    count = 0
-    for y in range(len(octopuses)):
-        for x in range(len(octopuses[y])):
-            if octopuses[y][x] > 9:
-                octopuses[y][x] = 0
-                count += 1
-    return count
-
-
 def octopuses_are_lid(octopuses):
-    print("Started checking")
-    for y in range(len(octopuses) - 1):
-        print("checking:", octopuses[y])
+    for y in range(len(octopuses)):
         for x in range(len(octopuses[y])):
             if octopuses[y][x] > 9:
                 return True
     return False
 
 
-flash_count = 0
-for step in range(0, 2):
-    print('After step', step, ':')
-    for i in range(len(octopuses) - 1):
-        print(octopuses[i])
+def turn_minus_zero(octopuses):
+    for y in range(len(octopuses)):
+        for x in range(len(octopuses[y])):
+            if octopuses[y][x] == -1:
+                octopuses[y][x] = 0
 
+
+def increment_octopus(y, x, octopuses):
+    if octopuses[y][x] != -1:
+        octopuses[y][x] += 1
+
+
+flash_count = 0
+print('Starting position:')
+for i in range(len(octopuses)):
+    print(octopuses[i])
+
+for day in range(1, 101):
     for y in range(len(octopuses)):
         for x in range(len(octopuses[y])):
             octopuses[y][x] += 1
 
-    print("Tussenstop")
-    for i in range(len(octopuses) - 1):
+    print("2:")
+    for i in range(len(octopuses)):
         print(octopuses[i])
 
     while octopuses_are_lid(octopuses):
-        print("Tussenstop_2")
-        for i in range(len(octopuses) - 1):
+
+        print('after 0')
+        for i in range(len(octopuses)):
             print(octopuses[i])
 
         for y in range(len(octopuses)):
             for x in range(len(octopuses[y])):
                 if octopuses[y][x] > 9:
+                    octopuses[y][x] = -1
+                    flash_count += 1
                     if y > 0:
-                        octopuses[y - 1][x] += 1
+                        increment_octopus(y - 1, x, octopuses)
                         if x > 0:
-                            octopuses[y - 1][x - 1] += 1
+                            increment_octopus(y - 1, x - 1, octopuses)
                         if x + 1 < len(octopuses[y]):
-                            octopuses[y - 1][x + 1] += 1
-                    if y + 1 < len(octopuses) - 1:
-                        octopuses[y + 1][x] += 1
+                            increment_octopus(y - 1, x + 1, octopuses)
+                    if y + 1 < len(octopuses):
+                        increment_octopus(y + 1, x, octopuses)
                         if x > 0:
-                            octopuses[y + 1][x - 1] += 1
+                            increment_octopus(y + 1, x - 1, octopuses)
                         if x + 1 < len(octopuses[y]):
-                            octopuses[y + 1][x + 1] += 1
+                            increment_octopus(y + 1, x + 1, octopuses)
                     if x > 0:
-                        octopuses[y][x - 1] += 1
+                        increment_octopus(y, x - 1, octopuses)
                     if x + 1 < len(octopuses[y]):
-                        octopuses[y][x + 1] += 1
-        print("Tussenstop_3")
-        for i in range(len(octopuses) - 1):
+                        increment_octopus(y, x + 1, octopuses)
+        print("Adrenaline:")
+        for i in range(len(octopuses)):
             print(octopuses[i])
-        flash_count += reset_flashed(octopuses)
-        print("Flash count:", flash_count)
+
+    turn_minus_zero(octopuses)
+
+    print('After day', day, ':')
+    for i in range(len(octopuses)):
+        print(octopuses[i])
 
 print('------------------------------------------------------------')
 for i in range(len(octopuses)):
